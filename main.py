0bfd21f7a5b30260d9e9e7292cc44991e81d1340
Your Name <you@example.com>
2023-08-23 17:39:36 -0300

    Select pdfs and select output

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import PyPDF2
 import os
+import tkinter as tk
+from tkinter import filedialog
 
-merger = PyPDF2.PdfMerger()
 
-file_list = os.listdir("pdf")
-file_list.sort()
+def select_pdf_files():
+    file_paths = filedialog.askopenfilenames(filetypes=[("PDF  Files","*.pdf")])
+    return file_paths
 
-for file in file_list:
-    if ".pdf" in file:
-        merger.append(f'pdf/{file}')
 
-merger.write("merged.pdf")
+def select_output_location():
+    output_path = filedialog.asksaveasfilename(defaultextension=".pdf",filetypes=[("PDF Files", "*.pdf")])
+    return output_path
 
+def merge_pdfs(pdfs_files, output_path):
+    merger = PyPDF2.PdfMerger()
+
+    for file in pdfs_files:
+            merger.append(file)
+
+    with open(output_path, "wb") as output_file:
+        merger.write(output_file)
+
+    print("PDFs merged successfully!")
+
+def main():
+    root = tk.Tk()
+    root.withdraw()
+
+    pdf_files = select_pdf_files()
+    if not pdf_files:
+        print("No PDFs selected.")
+        return
+    output_path = select_output_location()
+    if not output_path:
+        print("No output path selected.")
+        return
+    merge_pdfs(pdf_files,output_path)
+
+
+if __name__ == "__main__":
+    main()
